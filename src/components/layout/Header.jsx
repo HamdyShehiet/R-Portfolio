97f8a46d8996655050cd8561ba0fe08d3ad874d1
Hamdy <tantaacademyt196@gmail.com>
2025-02-22 12:26:47 +0000

    Update: Header Component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
                 <li><a href="#about">About</a></li>
                 <li><a href="#services">Services</a></li> 
                 <li><a href="#blog">Blog</a></li>
-                <li><a href="#cotact">Contact</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     </div>
